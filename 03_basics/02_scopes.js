38dd94b727ffee0289c170788522daf71462c6b4
Jiten Mistri <44949991+JitenMistri@users.noreply.github.com>
2024-04-03 17:40:48 +0000

    Nested function scopes and javascript Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,6 +12,29 @@ console.log(b);         // b is not defined
 // This is execute properly as var doesnt have scope
 console.log(c);         // 30
 
+function one() {
+    const name = "jiten"
+
+    function two() {
+        const age = 24
+        console.log(name);      // jiten -> global variables can be used inside any scope
+    }
+    // console.log(age);        // -> will give error as this is out of the scope of age
+
+    two()
+}
+
+one()
 
 
+// javascript hoisting
 
+addOne(3)       // can easily access
+function addOne(num) {
+    return num + 1
+}
+
+addTwo(3)        // This will give error as "Cannot access 'addTwo' before initializing" as we are holding this function in a variable
+const addTwo = function(num) {
+    return num + 2
+}
